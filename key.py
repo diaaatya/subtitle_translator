2cedd3c7beb62a5f9bafbacdf55c50bd4b8b8253
diaa atya <diaa.atya@hotmail.com>
2021-08-07 07:41:15 +0200

    first

---
+++ key.py
@@ -0,0 +1,1 @@
+key = "0aace837eemshb5d19a7fdadd9e7p154ebcjsn43ab4830b102"
---
+++ trans.py
@@ -0,0 +1,46 @@
+import requests
+import key
+
+url = "https://microsoft-translator-text.p.rapidapi.com/translate"
+
+querystring = {"api-version": "3.0", "to": "ar",
+               "textType": "plain", "profanityAction": "NoAction"}
+
+
+def translate(subtitle):
+    payload = "[\r\n    {\r\n        \"Text\": \""
+    payload2 = "\"\r\n    }\r\n]"
+    payload = payload+subtitle+payload2
+    headers = {
+        'content-type': "application/json",
+        'x-rapidapi-key': key.key,
+        'x-rapidapi-host': "microsoft-translator-text.p.rapidapi.com"
+    }
+    response = requests.request(
+        "POST", url, data=payload, headers=headers, params=querystring)
+
+    text = response.text
+    start = text.find('"text":"')
+    end = text.find('","to"')
+    text = text[start+len('"text":"'):end]
+    return text
+
+
+translated = []
+finished = False
+with open("shawshank-eng.srt") as f:
+    for line in f:
+        if line[0:1].lower() in "abcdefghijklmnopqrstuvwxyz":
+            line = translate(line.replace("\n", " "))
+            translated.append(line)
+            print(line)
+        else:
+            translated.append(line.replace("\n", " "))
+    finished = True
+
+if finished:
+    with open("shawshank-ar", "w", encoding="utf-8") as f:
+        for line in translated:
+            line.encode("utf-8")
+            f.write(line)
+            f.write("\n")
